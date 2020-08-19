df816cc03f1967e9fdfbfc9f1f25db5da21695f9
Guilherme H Noce <hidekinoce@gmail.com>
2020-08-18 22:12:00 -0300

    Resolução do Desafio

--- src/app.js
+++ src/app.js
@@ -23,9 +23,9 @@ return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
-  const repositorie = { id:uuid(), title, url, techs, likes};
+  const repositorie = { id:uuid(), title, url, techs, likes:0};
 
   repositories.push(repositorie);
 
@@ -35,19 +35,21 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const { title, url, techs, likes } = request.body;
+  const { title, url, techs } = request.body;
 
-  const repositorieIndex = repositories.findIndex(repositorie.id == id);
+  const repositorieIndex = repositories.findIndex( repositorie => repositorie.id == id);
 
   if (repositorieIndex < 0) {
-    return response.status(400).json({ error: 'Repositorie nor foud! '});
+    return response.status(400).json({ error: 'Repositorie not foud! '});
   };
-
+  likes = repositories[repositorieIndex].likes;
+  
   const repositorie = {
     id,
     title,
     url,
-    techs
+    techs,
+    likes
   };
 
   repositories[repositorieIndex] = repositorie;
@@ -57,11 +59,34 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not foud! '});
+  };
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
+
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  ;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not foud! '});
+  }     
+  
+  repositories[repositorieIndex].likes++;
+  
+  return response.json(repositories[repositorieIndex])
 });
 
 module.exports = app;
